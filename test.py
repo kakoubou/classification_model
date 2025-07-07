fdca642c0738b3b43cc2342067fd3aacc7918d0f
kakoubou <kakoubou1395@gmail.com>
2025-07-07 22:29:14 +0900

    update data structure and retrain model

--- test.py
+++ test.py
@@ -8,29 +8,31 @@ from pathlib import Path
 
 from utils import test_model
 
-# Load configuration file
-config_path = Path(__file__).parent / "config" / "config_train.yaml"
-with open(config_path, 'r') as file:
-    config = yaml.safe_load(file)
 
-# Define image preprocessing transforms
-transform = transforms.Compose([
-    transforms.Resize((500, 500)),
-    transforms.ToTensor(),
-])
+if __name__ == '__main__':
+    # Load configuration file
+    config_path = Path(__file__).parent / "config" / "config_train.yaml"
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
 
-# Load the model
-model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes=config["num_classes"])
-checkpoint_path = config["test_model_path"]
-model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('cpu')))
+    # Define image preprocessing transforms
+    transform = transforms.Compose([
+        transforms.Resize((500, 500)),
+        transforms.ToTensor(),
+    ])
 
-# Evaluate the model, generate and save confusion matrix and accuracy
-test_model(
-    model=model,
-    test_dataset_path=config["test_dataset_path"],
-    transform=transform,
-    batch_size=128,
-    save_dir="logs"
-)
+    # Load the model
+    model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes=config["num_classes"])
+    checkpoint_path = config["test_model_path"]
+    model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('cpu')))
+
+    # Evaluate the model, generate and save confusion matrix and accuracy
+    test_model(
+        model=model,
+        test_dataset_path=config["test_dataset_path"],
+        transform=transform,
+        batch_size=128,
+        save_dir="logs"
+    )
 
 
